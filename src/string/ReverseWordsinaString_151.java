12772f9057ac5a7cf282a30acc70b62d1764b90e
Xianjian Bao <xbao@musacompanies.com>
2018-11-30 23:59:43 -0600

    151, 206, 344

--- src/string/ReverseWordsinaString_151.java
+++ src/string/ReverseWordsinaString_151.java
@@ -12,22 +12,27 @@ package string;
 public class ReverseWordsinaString_151 {
     
     public String reverseWords(String s) {
-        if(s==null || s==""){return "";}
+        if(s==null || s.equals("")){return "";}
         
-        s=s.replaceAll("[ ]+", " ");
+        //s=s.replaceAll("[ ]+", " ");
         String[] words =s.split(" ");
-        String str="";
+        StringBuilder str=new StringBuilder();
         for(int i=words.length-1;i>=0;i--)
         {
-            str+=words[i]+" ";
+            if(words[i].equals("")) {
+                continue;
+            }
+            str.append(words[i]);
+            str.append(" ");
+          //  str+=words[i]+" ";
         }
         
-        return str.trim();
+        return str.toString().trim();
     }
     
-    public void reverseWordsTest()
+    public static void main(String[] args)
     {
-        String str=this.reverseWords("the sky    is bulue");
+        String str=new ReverseWordsinaString_151().reverseWords("the sky    is bulue");
         System.out.println(str);
     }
 
